19b72bbc12e5b660273532331b896f3ef9a35596
Malcolm Jack <inquisitorjax@gmail.com>
2016-11-09 00:24:27 +0200

    remove unece packages and fix android

--- Wibci.Xamarin.Images/Wibci.Xamarin.Images.Droid/AndroidResizeImageCommand.cs
+++ Wibci.Xamarin.Images/Wibci.Xamarin.Images.Droid/AndroidResizeImageCommand.cs
@@ -1,13 +1,7 @@
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Android.Graphics;
+using System.IO;
+using System.Threading.Tasks;
+using Wibci.LogicCommand;
 
 namespace Wibci.Xamarin.Images.Droid
 {
